eab75dfb80dc567ac735bda6303a5df33b5bd94c
Tobias Koppers <tobias.koppers@googlemail.com>
2012-04-06 12:28:34 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+/*
+	MIT License http://www.opensource.org/licenses/mit-license.php
+	Author Tobias Koppers @sokra
+*/
+module.exports = function() {
+	var args = Array.prototype.slice.call(arguments);
+	args = args.join("");
+	this.values = [args];
+	return "module.exports = " + JSON.stringify(args);
+}
