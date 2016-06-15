b2c186fbcaa63213b4dedc086217963f3c02b548
Larkin Family <sean.larkin@cuw.edu>
2016-06-15 00:32:29 -0500

    feat(sourcemaps) include sourcemap support for raw-loader

--- index.js
+++ index.js
@@ -2,8 +2,11 @@
 	MIT License http://www.opensource.org/licenses/mit-license.php
 	Author Tobias Koppers @sokra
 */
-module.exports = function(content) {
+module.exports = function(content, sourcemaps) {
 	this.cacheable && this.cacheable();
 	this.value = content;
-	return "module.exports = " + JSON.stringify(content);
+	var newContent = "module.exports = " + JSON.stringify(content);
+
+	this.callback && this.callback(null, newContent, sourcemaps);
+	return newContent;
 }
