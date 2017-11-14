78cb753ade7d1f465bbfe3b3c075bb55985656dc
jheyming <jheyming@cloudera.com>
2017-11-14 09:30:38 -0800

    Handle special whitespace characters.
    If your raw text has a string with Line separator or Paragraph separator,
    they need to be handled outside of JSON.stringify:
    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/stringify#Issue_with_plain_JSON.stringify_for_use_as_JavaScript
    
    The fix here is to replace those characters as directed in the MDN article above.
    
    I also changed the formatting from tabs to 2 spaces :-p

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 /*
-	MIT License http://www.opensource.org/licenses/mit-license.php
-	Author Tobias Koppers @sokra
+  MIT License http://www.opensource.org/licenses/mit-license.php
+  Author Tobias Koppers @sokra
 */
 module.exports = function(content) {
-	this.cacheable && this.cacheable();
-	this.value = content;
-	return "module.exports = " + JSON.stringify(content);
+  this.cacheable && this.cacheable();
+  this.value = content;
+
+  var stringified = JSON.stringify(content)
+  .replace(/\u2028/g, '\\u2028')
+  .replace(/\u2029/g, '\\u2029');
+
+  return "module.exports = " + stringified;
 }
