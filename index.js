c53a7dea0404363a563ab4790abcbbb68e3814c1
jheyming <jheyming@cloudera.com>
2017-11-15 09:37:14 -0800

    Address code review.

--- index.js
+++ index.js
@@ -2,13 +2,13 @@
   MIT License http://www.opensource.org/licenses/mit-license.php
   Author Tobias Koppers @sokra
 */
-module.exports = function(content) {
+module.exports = function(rawContent) {
   this.cacheable && this.cacheable();
-  this.value = content;
+  this.value = rawContent;
 
-  var stringified = JSON.stringify(content)
-  .replace(/\u2028/g, '\\u2028')
-  .replace(/\u2029/g, '\\u2029');
+  var stringified = JSON.stringify(rawContent)
+    .replace(/\u2028/g, '\\u2028')
+    .replace(/\u2029/g, '\\u2029');
 
   return "module.exports = " + stringified;
 }
