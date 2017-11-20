c56b5757c7e6a729173c9c1695ad80b843a58a9f
Joshua Wiens <wiens.joshua@gmail.com>
2017-11-19 18:18:53 -0600

    refactor: Apply `webpack-defaults` (#40)
    * refactor: Init defaults
    
    * refactor: Pick up changes from master

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
   MIT License http://www.opensource.org/licenses/mit-license.php
   Author Tobias Koppers @sokra
 */
-module.exports = function(source) {
+export default function (source) {
   this.value = source;
 
   var json = JSON.stringify(source)
@@ -10,4 +10,4 @@ module.exports = function(source) {
     .replace(/\u2029/g, '\\u2029');
 
   return 'export default ' + json;
-};
+}
