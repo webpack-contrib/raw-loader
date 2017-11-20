224bb81f6ab9688de4931eb494fc3d499c1ffdbd
Joshua Wiens <joshuaw@easymetrics.com>
2017-11-19 18:16:33 -0600

    refactor: Pick up changes from master

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@
   MIT License http://www.opensource.org/licenses/mit-license.php
   Author Tobias Koppers @sokra
 */
-export default function (content) {
-  this.value = content;
-  const sanitizeUnicode = JSON.stringify(content)
+export default function (source) {
+  this.value = source;
+
+  var json = JSON.stringify(source)
     .replace(/\u2028/g, '\\u2028')
     .replace(/\u2029/g, '\\u2029');
-  return `export default ${sanitizeUnicode}`;
+
+  return 'export default ' + json;
 }
