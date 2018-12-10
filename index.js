23a979edd403741b42d3f600204a429f21950d88
Frank Fang <FrankFang1990@gmail.com>
2018-12-10 17:03:43 +0800

    refactor: remove unused (#57)

--- index.js
+++ index.js
@@ -3,11 +3,8 @@
   Author Tobias Koppers @sokra
 */
 module.exports = function rawLoader(source) {
-  this.value = source;
-
   const json = JSON.stringify(source)
     .replace(/\u2028/g, '\\u2028')
     .replace(/\u2029/g, '\\u2029');
-
   return `module.exports = ${json}`;
 };
