74bb6bb3e68c08a9ec66198ff6f163c1bf50b6d4
Ivan Nikolić <niksy5@gmail.com>
2019-03-13 09:43:15 +0100

    refactor: use ES Module export instead of CommonJS

--- src/index.js
+++ src/index.js
@@ -12,5 +12,5 @@ export default function rawLoader(source) {
     .replace(/\u2028/g, '\\u2028')
     .replace(/\u2029/g, '\\u2029');
 
-  return `module.exports = ${json}`;
+  return `export default ${json}`;
 }
