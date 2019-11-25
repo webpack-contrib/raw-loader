5ec82d6c1fcfbbaebdb5b3d08cdad3719a5ffe2c
Evilebot Tnawi <evilebottnawi@users.noreply.github.com>
2019-11-25 17:49:08 +0300

    refactor: next (#88)

--- test/fixtures/basic.js
+++
@@ -1,7 +0,0 @@
-// eslint-disable-next-line import/no-webpack-loader-syntax, import/no-unresolved
-import inline from '!!../../src/index.js!./file.txt';
-
-import txt from './file.txt';
-import separator from './separator.txt';
-
-export { txt, separator, inline };
--- test/validate-options.test.js
+++ test/validate-options.test.js
@@ -1,18 +1,60 @@
-import loader from '../src';
+import { getCompiler, compile } from './helpers';
 
-it('validate options', () => {
-  const validate = (options) =>
-    loader.call(
-      Object.assign(
-        {},
-        {
-          query: options,
+describe('validate options', () => {
+  const tests = {
+    esModule: {
+      success: [true, false],
+      failure: ['true'],
+    },
+    unknown: {
+      success: [],
+      failure: [1, true, false, 'test', /test/, [], {}, { foo: 'bar' }],
+    },
+  };
+
+  function stringifyValue(value) {
+    if (
+      Array.isArray(value) ||
+      (value && typeof value === 'object' && value.constructor === Object)
+    ) {
+      return JSON.stringify(value);
+    }
+
+    return value;
+  }
+
+  async function createTestCase(key, value, type) {
+    it(`should ${
+      type === 'success' ? 'successfully validate' : 'throw an error on'
+    } the "${key}" option with "${stringifyValue(value)}" value`, async () => {
+      const compiler = getCompiler('simple.js', { [key]: value });
+
+      let stats;
+
+      try {
+        stats = await compile(compiler);
+      } finally {
+        if (type === 'success') {
+          expect(stats.hasErrors()).toBe(false);
+        } else if (type === 'failure') {
+          const {
+            compilation: { errors },
+          } = stats;
+
+          expect(errors).toHaveLength(1);
+          expect(() => {
+            throw new Error(errors[0].error.message);
+          }).toThrowErrorMatchingSnapshot();
         }
-      ),
-      'Text'
-    );
+      }
+    });
+  }
 
-  expect(() => validate({})).not.toThrow();
-  expect(() => validate(true)).not.toThrow();
-  expect(() => validate({ unknown: true })).toThrowErrorMatchingSnapshot();
+  for (const [key, values] of Object.entries(tests)) {
+    for (const type of Object.keys(values)) {
+      for (const value of values[type]) {
+        createTestCase(key, value, type);
+      }
+    }
+  }
 });
