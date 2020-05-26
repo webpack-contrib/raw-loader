f165db0e14b092c026dd663dd8ded06258bc691d
Nikolai Prokoschenko <nikolai.prokoschenko@kurzdigital.com>
2020-05-26 12:50:21 +0200

    feat: add test for binary files

---
+++ test/fixtures/binary.js
@@ -0,0 +1,5 @@
+import binary from './binary.txt';
+
+__export__ = binary;
+
+export default binary;
--- test/loader.test.js
+++ test/loader.test.js
@@ -1,3 +1,5 @@
+import crypto from 'crypto';
+
 import {
   compile,
   execute,
@@ -13,6 +15,42 @@ describe('loader', () => {
 
     expect(
       execute(readAsset('main.bundle.js', compiler, stats))
+    ).toMatchSnapshot('result');
+    expect(Object.keys(stats.compilation.assets)).toMatchSnapshot('assets');
+    expect(normalizeErrors(stats.compilation.warnings)).toMatchSnapshot(
+      'warnings'
+    );
+    expect(normalizeErrors(stats.compilation.errors)).toMatchSnapshot('errors');
+  });
+
+  it('should not work with binary', async () => {
+    const compiler = getCompiler('binary.js');
+    const stats = await compile(compiler);
+
+    expect(
+      crypto
+        .createHash('md5')
+        .update(execute(readAsset('main.bundle.js', compiler, stats)))
+        .digest('hex')
+    ).toMatchSnapshot('result');
+    expect(Object.keys(stats.compilation.assets)).toMatchSnapshot('assets');
+    expect(normalizeErrors(stats.compilation.warnings)).toMatchSnapshot(
+      'warnings'
+    );
+    expect(normalizeErrors(stats.compilation.errors)).toMatchSnapshot('errors');
+  });
+
+  it('should work with binary with raw option', async () => {
+    const compiler = getCompiler('binary.js', { raw: true });
+    const stats = await compile(compiler);
+
+    expect(
+      crypto
+        .createHash('md5')
+        .update(
+          Buffer.from(execute(readAsset('main.bundle.js', compiler, stats)))
+        )
+        .digest('hex')
     ).toMatchSnapshot('result');
     expect(Object.keys(stats.compilation.assets)).toMatchSnapshot('assets');
     expect(normalizeErrors(stats.compilation.warnings)).toMatchSnapshot(
